dd4df0897a2321dd822450926022f4df97cb5fc9
ndimitrova <nninata@abv.bg>
2018-03-17 22:25:54 +0200

    I added a file with name Rectangle Area.csproj.

---
+++ Rectangle Area/Rectangle Area/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var rectangleArea = width * height;
+
+            Console.WriteLine("{0:f2}", rectangleArea);
+        }
+    }
+}
