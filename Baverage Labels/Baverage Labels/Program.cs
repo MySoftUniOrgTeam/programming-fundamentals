a4a8dfeb81544c22e73d3c1839be77e7ce7dadeb
ndimitrova <nninata@abv.bg>
2018-03-17 22:33:54 +0200

    I added a file with name Baverage Labels.csproj.

---
+++ Baverage Labels/Baverage Labels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Baverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energyContent = int.Parse(Console.ReadLine());
+            var sugarContent = int.Parse(Console.ReadLine());
+
+            var energyContentGivenVolume = (volume * energyContent) / 100.0;
+            var sugarContentGivenVolume = (volume * sugarContent) / 100.0;
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", energyContentGivenVolume, sugarContentGivenVolume);
+        }
+    }
+}
