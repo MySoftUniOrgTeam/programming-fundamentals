2a7c2f1003f222b7af21ded73fa164d92078db54
ndimitrova <nninata@abv.bg>
2018-03-17 22:30:53 +0200

    I added a file with name Miles To Kilometres.csproj.

---
+++ Miles To Kilometres/Miles To Kilometres/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Miles_To_Kilometres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+
+            double kilometres = 1.60934 * miles;
+
+            Console.WriteLine("{0:f2}", kilometres);
+        }
+    }
+}
