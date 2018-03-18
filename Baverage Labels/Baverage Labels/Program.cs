cfb3a468c1a8b8293364d51091ad2d0e5ee714a9
MarioGeorgiev <mario_georgiev_kn@abv.bg>
2018-03-18 09:48:22 +0200

    Add new project

--- Baverage Labels/Baverage Labels/Program.cs
+++ Baverage Labels/Baverage Labels/Program.cs
@@ -6,15 +6,15 @@ namespace Baverage_Labels
     {
         static void Main(string[] args)
         {
-            var name = Console.ReadLine();
+            var name = Console.ReadLine(); // reading the input from the console 
             var volume = int.Parse(Console.ReadLine());
             var energyContent = int.Parse(Console.ReadLine());
             var sugarContent = int.Parse(Console.ReadLine());
 
-            var energyContentGivenVolume = (volume * energyContent) / 100.0;
+            var energyContentGivenVolume = (volume * energyContent) / 100.0; // calculate 
             var sugarContentGivenVolume = (volume * sugarContent) / 100.0;
 
-            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}ml {1}:", volume, name); // use place holders to out the result 
             Console.WriteLine("{0}kcal, {1}g sugars", energyContentGivenVolume, sugarContentGivenVolume);
         }
     }
