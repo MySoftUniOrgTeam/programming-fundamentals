c6217067a17426afb3bd9f714105fcb0e5d2d644
MarioGeorgiev <mario_georgiev_kn@abv.bg>
2018-03-18 09:48:22 +0200

    Add new project ;add comments on the code what exactly do .

--- Character Stats/Character Stats/Program.cs
+++ Character Stats/Character Stats/Program.cs
@@ -10,14 +10,14 @@ namespace Character_Stats
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
+            string name = Console.ReadLine(); // reading the input from the console 
             var currentHealth = int.Parse(Console.ReadLine());
             var maxHealth = int.Parse(Console.ReadLine());
             var currentEnergy = int.Parse(Console.ReadLine());
             var maxEnergy = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Health: {'|' + new string('|', currentHealth) + new string('.', (maxHealth - currentHealth)) + '|'}");
+            Console.WriteLine($"Health: {'|' + new string('|', currentHealth) + new string('.', (maxHealth - currentHealth)) + '|'}"); // use place holders to our the result 
             Console.WriteLine($"Energy: {'|' + new string('|', currentEnergy) + new string('.', (maxEnergy - currentEnergy)) + '|'}");
         }
     }
